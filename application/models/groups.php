f87974a18c928222494a60c91654940d58d17053
Colin Devroe <colin@cdevroe.com>
2013-12-26 12:35:01 -0500

    Started a groups model.

---
+++ application/models/groups.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Groupsmodel extends CI_Controller {
+
+	function __construct()
+    {
+        // Call the Model constructor
+        parent::__construct();
+    }
+
+    function group_create() {}
+
+}
