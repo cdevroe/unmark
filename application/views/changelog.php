dbecf054f0b8880d908db1c76ec5fd0d1cf18803
Colin Devroe <colin@cdevroe.com>
2014-01-09 09:01:53 -0500

    Updated changelog.

--- application/views/changelog.php
+++ application/views/changelog.php
@@ -5,9 +5,10 @@
     <h2>Changelog</h2>
 		<p>A quick list of what's new. Posted immediately upon update.</p>
 
-		<h4 id="035"><a href="#035">0.3.5 - UNRELEASED</a></h4>
+		<h4 id="035"><a href="#035">0.3.5 - 9 January 2014</a></h4>
 		  <ul>
 		    <li>Update: Removed all references to nilai.co from the app and used $config['base_url'] instead.</li>
+		    <li>Fix: Quick patch for retina Macbook Pros.</li>
 		  </ul>
 
 		<h4 id="033"><a href="#032">0.3.3 - 6 January 2014</a></h4>
