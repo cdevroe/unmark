00bfd3f79e8b5a8632b91aeb00432d358b1c44cf
Jeff Johns <phpfunk@gmail.com>
2014-01-14 16:41:07 -0500

    Updated MY_ config to Plain_

--- application/config/config.php
+++ application/config/config.php
@@ -106,7 +106,7 @@ $config['enable_hooks'] = FALSE;
 | http://codeigniter.com/user_guide/general/creating_libraries.html
 |
 */
-$config['subclass_prefix'] = 'MY_';
+$config['subclass_prefix'] = 'Plain_';
 
 
 /*
