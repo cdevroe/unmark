d80750b7826f88a07f922c15e151e939d3d09142
Jeff Johns <phpfunk@gmail.com>
2014-01-15 09:45:46 -0500

    Updated migration

--- application/migrations/004_password_update.php
+++ application/migrations/004_password_update.php
@@ -4,7 +4,7 @@ class Migration_Users_update extends CI_Migration {
 
     public function up()
     {
-      $this->db->query("ALTER TABLE `users` ADD COLUMN `salt` varchar(50) NOT NULL COMMENT 'The salt used to generate password.' AFTER `password`");
+      $this->db->query("ALTER TABLE `users` ADD COLUMN `salt` varchar(50) DEFAULT NULL COMMENT 'The salt used to generate password.' AFTER `password`");
     }
 
     public function down()
