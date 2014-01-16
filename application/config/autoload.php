359eade294d4a27423fdcd6323d4bd92e8670055
Jeff Johns <phpfunk@gmail.com>
2014-01-16 15:54:15 -0500

    autoload data helper

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -64,7 +64,7 @@ $autoload['libraries'] = array('database','cache');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array();
+$autoload['helper'] = array('data_helper');
 
 
 /*
