5ae982837abb2b04d210d0a87859a2edd848a230
Jeff Johns <phpfunk@gmail.com>
2014-01-21 11:26:25 -0500

    Updated labels.edit method

--- application/controllers/labels.php
+++ application/controllers/labels.php
@@ -17,9 +17,9 @@ class Labels extends Plain_Controller
         - Start to return all labels that belong to user
         - Admins will see other labels that say user_id = null
         - applicable domains:
-            /api/labels?/smart(/PAGE)?  = only smart labels
-            /api/labels?/normal(/PAGE)? = only normal system level labels (admins only, non-admins get nothing)
-            /api/labels?(/PAGE)?        = all labels (non-admins won't see system level labels)
+            /api/labels/smart(/PAGE)?  = only smart labels
+            /api/labels/normal(/PAGE)? = only normal system level labels (admins only, non-admins get nothing)
+            /api/labels(/PAGE)?        = all labels (non-admins won't see system level labels)
 
         - The user will only get their labels
         - Admins will get their labels and system level labels
@@ -62,31 +62,110 @@ class Labels extends Plain_Controller
         $this->figureView();
     }
 
-    // Add a new label (smart or regular)
+    /*
+        - Activate a single lable
+        - URLS
+            /api/label/add
+    */
     public function add()
     {
 
     }
 
-    // Add a new label (smart or regular)
+     /*
+        - Activate a single lable
+        - URLS
+            /api/label/activate/LABEL_ID
+    */
     public function activate($label_id=0)
     {
         self::toggle($label_id, 1);
     }
 
-    // Add a new label (smart or regular)
+    /*
+        - Deactivate a single lable
+        - URLS
+            /api/label/deactivate/LABEL_ID
+    */
     public function deactivate($label_id=0)
     {
         self::toggle($label_id, 0);
     }
 
-    // Edit an existing label
+   /*
+        - Edit a single label
+        - URLS
+            /api/label/edit/LABEL_ID
+
+        // For normal labels (admins only)
+         - name   : Optional
+         - active : Optional
+
+        // For smart labels
+         - domain : Optional
+         - path   : Optional
+         - active : Optional
+
+        // Easier to use activate/deactivate for this but you can still use the edit metho
+    */
     public function edit($label_id=0)
     {
 
+        // Figure correct way to handle if no mark id
+        if (empty($label_id) || ! is_numeric($label_id)) {
+            $this->data['errors'] = formatErrors('No `label_id` was found.');
+        }
+        else {
+            $types = array(
+                'label' => array('name', 'active'),
+                'smart' => array('domain', 'path', 'active')
+            );
+
+            // Lookup label_id to get type
+            $where = (parent::isAdmin() === true) ? "(labels.user_id IS NULL OR labels.user_id = '" . $this->user_id . "')" : "labels.user_id = '" . $this->user_id . "'";
+            $where .= " AND labels.label_id= '" . $label_id . "'";
+            $label = $this->labels->readComplete($where, 1);
+
+            if (! isset($label->type)) {
+                $this->data['errors'] = formatErrors('No label found using `' . $label_id . '` for your account.');
+            }
+            elseif (! array_key_exists($label->type, $types)) {
+                $this->data['errors'] = formatErrors('This type of label `' . $label->type . '` could not be found.');
+            }
+            else {
+                $options = array();
+                foreach ($types[$label->type] as $k => $column) {
+                    if (isset($this->db_clean->{$column})) {
+                        $options[$column] = $this->db_clean->{$column};
+                    }
+                }
+
+                if (empty($options)) {
+                    $this->data['errors'] = formatErrors('No options found to update for this label.');
+                }
+                else {
+                    $label = $this->labels->update($where, $options);
+                    if ($label === false) {
+                        $this->data['errors'] = formatErrors('Label could not be updated.');
+                    }
+                    else {
+                        $this->data['label'] = $label;
+                    }
+                }
+            }
+
+        }
+
+
+        // Figure view
+        $this->figureView();
     }
 
-    // Lookup info for a label
+    /*
+        - Get info for a single label
+        - URLS
+            /api/label/info/LABEL_ID
+    */
     public function info($label_id=0)
     {
         // Figure correct way to handle if no mark id
