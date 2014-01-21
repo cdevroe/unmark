9cc08471a8e65adbf9c6148368698b9b4ca73b9b
Jeff Johns <phpfunk@gmail.com>
2014-01-21 10:53:08 -0500

    Optimized labels.index Added some comments Allow users to get normal, smart or all labels on this call

--- application/controllers/labels.php
+++ application/controllers/labels.php
@@ -13,19 +13,44 @@ class Labels extends Plain_Controller
         $this->load->model('labels_model', 'labels');
     }
 
-    // Start to return all labels that belong to user
-    // Admins will see other labels that say user_id = null
-    public function index($page=1)
-    {
-        // Figure the correct starting page
-        $page = (! is_numeric($page) || $page < 0) ? 1 : $page;
+    /*
+        - Start to return all labels that belong to user
+        - Admins will see other labels that say user_id = null
+        - applicable domains:
+            /api/labels?/smart(/PAGE)?  = only smart labels
+            /api/labels?/normal(/PAGE)? = only normal system level labels (admins only, non-admins get nothing)
+            /api/labels?(/PAGE)?        = all labels (non-admins won't see system level labels)
 
-        // Set where
-        // If admin account, get system labels
-        $where = "labels.user_id='". $this->user_id . "'";
-        if (parent::isAdmin() === true) {
-            $where .= " OR labels.user_id IS NULL";
+        - The user will only get their labels
+        - Admins will get their labels and system level labels
+    */
+    public function index($v1=0, $v2=null)
+    {
+
+        // Figure the type
+        $type = (! is_numeric($v1)) ? $v1 : 'all';
+
+        // Figure the correct starting page
+        $page = (! is_numeric($v1) || $v1 < 0) ? 1 : $v1;
+        $page = (is_numeric($v2) || $v2 > 0) ? $v2 : $page;
+
+        // Set the where
+        $where = null;
+
+        // If $type != all, set a where
+        if ($type != 'all') {
+            $where = ($type == 'normal') ? "labels.smart_label_id IS NULL " : "labels.smart_label_id IS NOT NULL ";
         }
+
+        // Set user where
+        // If admin account, get normal labels
+        $user_where = "labels.user_id='". $this->user_id . "'";
+        if (parent::isAdmin() === true) {
+            $user_where .= " OR labels.user_id IS NULL";
+        }
+
+        // Set final where
+        $where = (empty($where)) ? $user_where : $where . ' AND (' . $user_where . ')';
 
         // Get current page, total pages and total records
         $this->data = $this->labels->getTotals($where, $page, $this->limit, $this->data);
@@ -60,6 +85,8 @@ class Labels extends Plain_Controller
     {
 
     }
+
+    // Get all normal or smart labels
 
     // Lookup info for a label
     public function info($label_id=0)
