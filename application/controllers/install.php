e0cee3271e7c0452e09451f7a42fac95d66ddfb7
Jeff Johns <phpfunk@gmail.com>
2014-01-23 10:31:59 -0500

    Updated install controller to check if admin in constructor

--- application/controllers/install.php
+++ application/controllers/install.php
@@ -1,15 +1,12 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Install extends CI_Controller {
+class Install extends Plain_Controller
+{
 
-  public function __construct() {
+  public function __construct()
+  {
     parent::__construct();
-
-    $this->load->helper(array('url','form'));
-    $this->load->library('session');
-
-     // Load Database for all methods
-
+    parent::redirectIfNotAdmin();
   }
 
   public function index()
@@ -57,43 +54,17 @@ class Install extends CI_Controller {
   // Used to update from one version to another.
   public function upgrade()
   {
-      // Logged in user id
-      //$userId = $_SESSION['user']['user_id'];
-      //if (!empty($userId)) {
-          //$userFromDb = $this->db->get_where('users',array('user_id' => $userId));
-          //if($userFromDb->num_rows() > 0){
-             // if($this->is_admin($userFromDb->row())){
-                  // Admin user logged in - run migrations if needed
-                  $this->load->library('migration');
 
-                  if ( ! $this->migration->current() )
-                  {
-                    show_error($this->migration->error_string());
-                    exit;
-                  }
-                  exit('Upgraded. Please <a href="/">return home</a>.');
-              //}
-          //}
-        //exit('You need to be logged in as admin to upgrade');
-      //} else {
-          // No logged user - redirect
-          //redirect('/');
-      //}
+    $this->load->library('migration');
+
+    if ( ! $this->migration->current() )
+    {
+      show_error($this->migration->error_string());
+      exit;
+    }
+    exit('Upgraded. Please <a href="/">return home</a>.');
+
   }
 
-  /**
-   * Checks if given user is an admin
-   * TODO Move function to more proper place (model? helper?)
-   * @param unknown $user
-   */
-  public function is_admin($user)
-  {
-      // TODO Add admin flag in DB to handle this
-      if(property_exists($user, 'admin')) {
-          return $user->admin;
-      } else {
-        return $user->user_id == 1;
-      }
-  }
 
  }
