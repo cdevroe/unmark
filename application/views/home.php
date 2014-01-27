a7acf44255cfccafbba651a824cdd59724697eb1
Tim Whitacre <tim@argyllstudios.com>
2014-01-27 09:37:15 -0500

    Added login file

---
+++ application/views/home.php
@@ -0,0 +1,6 @@
+<form method="post" action="/login" class="form-inline">
+    <input type="hidden" name="csrf_token" id="csrf_token" value="<?php print $csrf_token; ?>">
+    <input type="text" class="input-small" name="email" id="email" placeholder="Email Address">
+    <input type="password" class="input-small" name="password" id="password" placeholder="Password">
+    <input type="submit" value="Log in" name="login" id="login" class="btn">
+</form>
