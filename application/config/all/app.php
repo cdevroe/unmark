62e46b084661010d01986724ccd4263112ae4ab0
Jeff Johns <phpfunk@gmail.com>
2014-01-29 13:27:22 -0500

    Added new configuration for the app

---
+++ application/config/all/app.php
@@ -0,0 +1,12 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+ - Array of titles & urls to add to all newly registered accounts
+
+ These will be added to every new account so when they register they
+ have some links to *do* something with. The label of 'Do' will be added automatically.
+*/
+$config['new_account_links'] = array(
+    'Read Nilai\'s FAQ' => 'http://nilai.co/help/faq',
+    'How To Use Nilai'  => 'http://nilai.co/help/how'
+);
