b270a747ce4784753edde5785fbba1c0a6cb3e8f
Jeff Johns <phpfunk@gmail.com>
2014-01-29 15:44:55 -0500

    Alphabetize methods

--- application/controllers/marks.php
+++ application/controllers/marks.php
@@ -73,193 +73,6 @@ class Marks extends Plain_Controller
         $this->figureView('marks/archive');
     }
 
-    // The index of the marks page
-    public function index()
-    {
-
-        /*
-        - Figure the state of things by URL
-            URLS:
-                /marks
-                /marks/VALID_LOOKUP_KEY
-                /marks/DATE(/DATE)?
-                /marks/archive
-                /marks/search*?q=QUERY
-                /marks/tag*
-                /marks/label*
-        */
-
-        // Get URI segments
-        $segments = $this->uri->segment_array();
-        $lookup   = (isset($segments[2]) && ! empty($segments[2])) ? strtolower(trim(urldecode($segments[2]))) : 'all';
-        $finish   = (isset($segments[3]) && ! empty($segments[3])) ? strtolower(trim(urldecode($segments[3]))) : null;
-
-        // Set allowable textual starts
-        $valid_lookups = array(
-            'all'               => array('start' => null, 'finish' => null),
-            'archive'           => array('start' => null, 'finish' => null),
-            'search'            => array('start' => null, 'finish' => null),
-            'today'             => array('start' => strtotime('today'), 'finish' => strtotime('today')),
-            'yesterday'         => array('start' => strtotime('yesterday'), 'finish' => strtotime('yesterday')),
-            'last-week'         => array('start' => strtotime('-1 week'), 'finish' => strtotime('today')),
-            'last-month'        => array('start' => strtotime('-1 month'), 'finish' => strtotime('today')),
-            'last-three-months' => array('start' => strtotime('-3 months'), 'finish' => strtotime('today')),
-            'last-six-months'   => array('start' => strtotime('-6 months'), 'finish' => strtotime('today')),
-            'last-year'         => array('start' => strtotime('-1 year'), 'finish' => strtotime('today')),
-            'ages-ago'          => array('start' => strtotime('-20 years'), 'finish' => strtotime('-1 year'))
-        );
-
-        // If $lookup is one of the following, search by time is disabled
-        $no_time = array('all', 'archive', 'search');
-
-        $options = array();
-
-        // Figure when
-        $where_time = null;
-        if (array_key_exists($lookup, $valid_lookups)) {
-            $where_time .= (! in_array($lookup, $no_time)) ? " AND UNIX_TIMESTAMP(users_to_marks.created_on) >= '" . $valid_lookups[$lookup]['start'] . "'" : '';
-            $where_time .= (! in_array($lookup, $no_time)) ? " AND UNIX_TIMESTAMP(users_to_marks.created_on) <= '" . $valid_lookups[$lookup]['finish'] . "'" : '';
-            $this->data['lookup_type'] = $lookup;
-        }
-
-        // Label Lookups
-        // Get label ID if need be
-        // Set where and lookup type
-        elseif ($lookup == 'label') {
-
-            // Get label ID
-            $label_id = $finish;
-            if (! is_numeric($label_id)) {
-                $this->load->model('labels_model', 'label');
-                $label    = $this->label->read("slug = '" . $this->db->escape_str($label_id) . "'", 1, 1, 'label_id, name');
-                $label_id = (isset($label->label_id)) ? $label->label_id : 0;
-                $label_name = (isset($label->name)) ? $label->name : 0;
-            }
-
-            // Set the new where clause
-            // Set lookup type
-            $where_time                = " AND users_to_marks.label_id = '" . $label_id . "'";
-            $this->data['lookup_type'] = 'label';
-        }
-
-        // Tag lookups
-        // Get tag id if need be
-        // Set lookup type
-        // Set options to pass to readComplete
-        // readComplete will see that tag_id was sent and add an additional INNER JOIN
-        elseif ($lookup == 'tag') {
-            // Get label ID
-            $tag_id = $finish;
-            if (! is_numeric($tag_id)) {
-                $this->load->model('tags_model', 'tag');
-                $tag      = $this->tag->read("slug = '" . $this->db->escape_str($tag_id) . "'", 1, 1, '*');
-                $tag_id   = (isset($tag->tag_id)) ? $tag->tag_id : 0;
-                $tag_slug = (isset($tag->slug)) ? $tag->slug : null;
-            }
-
-            // Set the new where clause
-            // Set lookup type
-            $this->data['lookup_type'] = 'tag';
-            $options['tag_id'] = $tag_id;
-        }
-
-        // Date Range Search
-        else {
-            // Check for valid dates
-            $dates       = findStartFinish($lookup, $finish);
-            $where_time .= " AND UNIX_TIMESTAMP(users_to_marks.created_on) >= '" . $dates['start'] . "'";
-            $where_time .= " AND UNIX_TIMESTAMP(users_to_marks.created_on) <= '" . $dates['finish'] . "'";
-            $this->data['lookup_type'] = 'custom_date';
-        }
-
-        // Figure the page number
-        $page = findPage();
-
-        // Archives
-        $archive = ($lookup == 'archive') ? 'IS NOT NULL' : 'IS NULL';
-
-        // Search it up
-        $search = null;
-        if (isset($this->db_clean->q) && ! empty($this->db_clean->q)) {
-            $search = " AND users_to_marks.notes LIKE '%" . $this->db_clean->q . "%'";
-            $options['search']  = $this->db_clean->q;
-            $options['user_id'] = $this->user_id;
-        }
-
-        // Set where
-        $where = "users_to_marks.user_id='". $this->user_id . "' AND users_to_marks.archived_on " . $archive . $where_time . $search;
-
-        // Get all the marks
-        $marks = $this->user_marks->readComplete($where, $this->limit, $page, null, $options);
-
-        // Check for marks
-        // If false, return error; set total to 0
-        if ($marks === false) {
-            $this->data['errors'] = formatErrors('No marks found.', 12);
-            $this->data['total']  = 0;
-        }
-        // If not false
-        // Set the marks
-        // Check for a JOIN to send to the getTotals call
-        // Get the totals
-        else {
-
-            // Autolink marks
-            foreach ($marks as $k => $mark) {
-                if (isset($mark->tags) && ! empty($mark->tags)) {
-                    foreach ($mark->tags as $kk => $tag) {
-                        $marks[$k]->notes = str_replace('#' . $tag['slug'], '<a href="/marks/tag/' . $tag['slug'] . '">#' . $tag['slug'] . '</a>', $marks[$k]->notes);
-                    }
-                }
-            }
-
-            // Set marks
-            $this->data['marks'] = $marks;
-
-            // If a search, get totals here
-            if (isset($options['search'])) {
-                $this->data = $this->user_marks->getTotalsSearch($page, $this->limit, $this->data, $options['search'], $options['user_id']);
-            }
-            // Everthing else here
-            else {
-                $join       = (isset($options['tag_id']) && ! empty($options['tag_id'])) ? "INNER JOIN user_marks_to_tags UMTT ON users_to_marks.users_to_mark_id = UMTT.users_to_mark_id AND UMTT.tag_id = '" . $options['tag_id'] . "'" : null;
-                $this->data = $this->user_marks->getTotals($where, $page, $this->limit, $this->data, $join);
-            }
-        }
-
-        // If web view
-        // Get stats, labels and tags
-        // else skip this section and just return the marks
-        if (parent::isWebView() === true) {
-            self::getStats();
-            self::getLabels();
-            self::getTags();
-
-            // If looking up by label, set the current label
-            if ($lookup == 'label') {
-                foreach ($this->data['labels'] as $k => $label) {
-                    $this->data['labels'][$k]->current = ($label->label_id == $label_id) ? '1' : '0';
-                }
-
-                // Give Tim Tim his Active Label Array already!
-                $this->data['active_label'] = (isset($label_id)) ? array('label_id' => $label_id, 'label_name' =>$label_name) : array();
-            }
-
-            // If looking up by tag, set the current tag if applicable
-            if ($lookup == 'tag') {
-                $tag_keys = array('popular', 'recent');
-                foreach ($tag_keys as $key) {
-                    foreach ($this->data['tags'][$key] as $k => $tag) {
-                        $this->data['tags'][$key][$k]->current = ($tag->tag_id == $tag_id) ? '1' : '0';
-                    }
-                }
-            }
-        }
-
-        // Figure if web, redirect, internal ajax call or API
-        $this->figureView('marks/index');
-    }
-
     // Edit a mark
     // Both API and web view
     public function edit($mark_id=0)
@@ -448,6 +261,193 @@ class Marks extends Plain_Controller
         $this->load->model('user_marks_to_tags_model', 'user_tags');
         $this->data['tags']['popular'] = $this->user_tags->getPopular($this->user_id);
         $this->data['tags']['recent']  = $this->user_tags->getMostRecent($this->user_id);
+    }
+
+    // The index of the marks page
+    public function index()
+    {
+
+        /*
+        - Figure the state of things by URL
+            URLS:
+                /marks
+                /marks/VALID_LOOKUP_KEY
+                /marks/DATE(/DATE)?
+                /marks/archive
+                /marks/search*?q=QUERY
+                /marks/tag*
+                /marks/label*
+        */
+
+        // Get URI segments
+        $segments = $this->uri->segment_array();
+        $lookup   = (isset($segments[2]) && ! empty($segments[2])) ? strtolower(trim(urldecode($segments[2]))) : 'all';
+        $finish   = (isset($segments[3]) && ! empty($segments[3])) ? strtolower(trim(urldecode($segments[3]))) : null;
+
+        // Set allowable textual starts
+        $valid_lookups = array(
+            'all'               => array('start' => null, 'finish' => null),
+            'archive'           => array('start' => null, 'finish' => null),
+            'search'            => array('start' => null, 'finish' => null),
+            'today'             => array('start' => strtotime('today'), 'finish' => strtotime('today')),
+            'yesterday'         => array('start' => strtotime('yesterday'), 'finish' => strtotime('yesterday')),
+            'last-week'         => array('start' => strtotime('-1 week'), 'finish' => strtotime('today')),
+            'last-month'        => array('start' => strtotime('-1 month'), 'finish' => strtotime('today')),
+            'last-three-months' => array('start' => strtotime('-3 months'), 'finish' => strtotime('today')),
+            'last-six-months'   => array('start' => strtotime('-6 months'), 'finish' => strtotime('today')),
+            'last-year'         => array('start' => strtotime('-1 year'), 'finish' => strtotime('today')),
+            'ages-ago'          => array('start' => strtotime('-20 years'), 'finish' => strtotime('-1 year'))
+        );
+
+        // If $lookup is one of the following, search by time is disabled
+        $no_time = array('all', 'archive', 'search');
+
+        $options = array();
+
+        // Figure when
+        $where_time = null;
+        if (array_key_exists($lookup, $valid_lookups)) {
+            $where_time .= (! in_array($lookup, $no_time)) ? " AND UNIX_TIMESTAMP(users_to_marks.created_on) >= '" . $valid_lookups[$lookup]['start'] . "'" : '';
+            $where_time .= (! in_array($lookup, $no_time)) ? " AND UNIX_TIMESTAMP(users_to_marks.created_on) <= '" . $valid_lookups[$lookup]['finish'] . "'" : '';
+            $this->data['lookup_type'] = $lookup;
+        }
+
+        // Label Lookups
+        // Get label ID if need be
+        // Set where and lookup type
+        elseif ($lookup == 'label') {
+
+            // Get label ID
+            $label_id = $finish;
+            if (! is_numeric($label_id)) {
+                $this->load->model('labels_model', 'label');
+                $label    = $this->label->read("slug = '" . $this->db->escape_str($label_id) . "'", 1, 1, 'label_id, name');
+                $label_id = (isset($label->label_id)) ? $label->label_id : 0;
+                $label_name = (isset($label->name)) ? $label->name : 0;
+            }
+
+            // Set the new where clause
+            // Set lookup type
+            $where_time                = " AND users_to_marks.label_id = '" . $label_id . "'";
+            $this->data['lookup_type'] = 'label';
+        }
+
+        // Tag lookups
+        // Get tag id if need be
+        // Set lookup type
+        // Set options to pass to readComplete
+        // readComplete will see that tag_id was sent and add an additional INNER JOIN
+        elseif ($lookup == 'tag') {
+            // Get label ID
+            $tag_id = $finish;
+            if (! is_numeric($tag_id)) {
+                $this->load->model('tags_model', 'tag');
+                $tag      = $this->tag->read("slug = '" . $this->db->escape_str($tag_id) . "'", 1, 1, '*');
+                $tag_id   = (isset($tag->tag_id)) ? $tag->tag_id : 0;
+                $tag_slug = (isset($tag->slug)) ? $tag->slug : null;
+            }
+
+            // Set the new where clause
+            // Set lookup type
+            $this->data['lookup_type'] = 'tag';
+            $options['tag_id'] = $tag_id;
+        }
+
+        // Date Range Search
+        else {
+            // Check for valid dates
+            $dates       = findStartFinish($lookup, $finish);
+            $where_time .= " AND UNIX_TIMESTAMP(users_to_marks.created_on) >= '" . $dates['start'] . "'";
+            $where_time .= " AND UNIX_TIMESTAMP(users_to_marks.created_on) <= '" . $dates['finish'] . "'";
+            $this->data['lookup_type'] = 'custom_date';
+        }
+
+        // Figure the page number
+        $page = findPage();
+
+        // Archives
+        $archive = ($lookup == 'archive') ? 'IS NOT NULL' : 'IS NULL';
+
+        // Search it up
+        $search = null;
+        if (isset($this->db_clean->q) && ! empty($this->db_clean->q)) {
+            $search = " AND users_to_marks.notes LIKE '%" . $this->db_clean->q . "%'";
+            $options['search']  = $this->db_clean->q;
+            $options['user_id'] = $this->user_id;
+        }
+
+        // Set where
+        $where = "users_to_marks.user_id='". $this->user_id . "' AND users_to_marks.archived_on " . $archive . $where_time . $search;
+
+        // Get all the marks
+        $marks = $this->user_marks->readComplete($where, $this->limit, $page, null, $options);
+
+        // Check for marks
+        // If false, return error; set total to 0
+        if ($marks === false) {
+            $this->data['errors'] = formatErrors('No marks found.', 12);
+            $this->data['total']  = 0;
+        }
+        // If not false
+        // Set the marks
+        // Check for a JOIN to send to the getTotals call
+        // Get the totals
+        else {
+
+            // Autolink marks
+            foreach ($marks as $k => $mark) {
+                if (isset($mark->tags) && ! empty($mark->tags)) {
+                    foreach ($mark->tags as $kk => $tag) {
+                        $marks[$k]->notes = str_replace('#' . $tag['slug'], '<a href="/marks/tag/' . $tag['slug'] . '">#' . $tag['slug'] . '</a>', $marks[$k]->notes);
+                    }
+                }
+            }
+
+            // Set marks
+            $this->data['marks'] = $marks;
+
+            // If a search, get totals here
+            if (isset($options['search'])) {
+                $this->data = $this->user_marks->getTotalsSearch($page, $this->limit, $this->data, $options['search'], $options['user_id']);
+            }
+            // Everthing else here
+            else {
+                $join       = (isset($options['tag_id']) && ! empty($options['tag_id'])) ? "INNER JOIN user_marks_to_tags UMTT ON users_to_marks.users_to_mark_id = UMTT.users_to_mark_id AND UMTT.tag_id = '" . $options['tag_id'] . "'" : null;
+                $this->data = $this->user_marks->getTotals($where, $page, $this->limit, $this->data, $join);
+            }
+        }
+
+        // If web view
+        // Get stats, labels and tags
+        // else skip this section and just return the marks
+        if (parent::isWebView() === true) {
+            self::getStats();
+            self::getLabels();
+            self::getTags();
+
+            // If looking up by label, set the current label
+            if ($lookup == 'label') {
+                foreach ($this->data['labels'] as $k => $label) {
+                    $this->data['labels'][$k]->current = ($label->label_id == $label_id) ? '1' : '0';
+                }
+
+                // Give Tim Tim his Active Label Array already!
+                $this->data['active_label'] = (isset($label_id)) ? array('label_id' => $label_id, 'label_name' =>$label_name) : array();
+            }
+
+            // If looking up by tag, set the current tag if applicable
+            if ($lookup == 'tag') {
+                $tag_keys = array('popular', 'recent');
+                foreach ($tag_keys as $key) {
+                    foreach ($this->data['tags'][$key] as $k => $tag) {
+                        $this->data['tags'][$key][$k]->current = ($tag->tag_id == $tag_id) ? '1' : '0';
+                    }
+                }
+            }
+        }
+
+        // Figure if web, redirect, internal ajax call or API
+        $this->figureView('marks/index');
     }
 
     // Mark detail view
