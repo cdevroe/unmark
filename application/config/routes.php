7308436bdfd21ce6345d9fd3e66a05c6ec1132fa
Jeff Johns <phpfunk@gmail.com>
2014-01-29 15:40:58 -0500

    udpated add mark route to also accept marks/add instead of just mark/add

--- application/config/routes.php
+++ application/config/routes.php
@@ -54,7 +54,7 @@ $route['label/info(.*?)']        = 'labels/info$1';
 $route['labels?(.*?)']           = 'labels/index$1';
 
 // Single Mark Actions
-$route['mark/add(.*?)']          = 'marks/add$1';
+$route['marks?/add(.*?)']          = 'marks/add$1';
 $route['mark/archive(.*?)']      = 'marks/archive$1';
 $route['mark/edit(.*?)']         = 'marks/edit$1';
 $route['mark/info(.*?)']         = 'marks/info$1';
