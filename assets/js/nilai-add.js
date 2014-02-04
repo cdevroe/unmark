959c2f4f91fa11ed055cde627cc7659105eac603
Tim Whitacre <tim@argyllstudios.com>
2014-02-04 13:04:47 -0500

    Fixed up the styles and label view

--- assets/js/nilai-add.js
+++ assets/js/nilai-add.js
@@ -10,6 +10,12 @@
 
         function built_label_list(res) {
             console.log(res);
+            var key, labels = res.labels, obj, list = '';
+            for (key in labels) {
+               obj = labels[key];
+               list += '<li class="label-'+ obj['label_id'] +'"><a href="#" rel="'+ obj['label_id'] +'">'+ obj['name'] +'</a></li>';
+            }
+            $('ul.label-choices').prepend(list);
         };
 
         // Grab the labels list
@@ -22,13 +28,3 @@
 
 
 }(window.jQuery));
-
-
-
-/*<li class="label-2"><a href="#" rel="2">Read</a></li>
-<li class="label-3"><a href="#" rel="3">Watch</a></li>
-<li class="label-4"><a href="#" rel="4">Listen</a></li>
-<li class="label-5"><a href="#" rel="5">Buy</a></li>
-<li class="label-6"><a href="#" rel="6">Eat &amp; Drink</a></li>
-<li class="label-7"><a href="#" rel="7">Do</a></li>
-<li class="label-1"><a href="#" rel="1">Unlabeled</a></li>*/
