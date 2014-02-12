232a2c7b83c74e580e0118f6af1d1e6e886b7e77
Jeff Johns <phpfunk@gmail.com>
2014-02-12 12:24:02 -0500

    Update Plain Loader to add CUSTOMPATH first to look for libraries, models, helpers and views

--- application/core/Plain_Loader.php
+++ application/core/Plain_Loader.php
@@ -5,5 +5,22 @@ class Plain_Loader extends CI_Loader
     public function __construct()
     {
         parent::__construct();
+
+        // Add custom path information for libraries, helpers, models and views
+        log_message('debug', 'Plain_Loader Class Initialized');
+        array_unshift($this->_ci_library_paths, CUSTOMPATH);
+        array_unshift($this->_ci_helper_paths, CUSTOMPATH);
+        array_unshift($this->_ci_model_paths, CUSTOMPATH);
+        $this->_ci_view_paths = array(
+            CUSTOMPATH . 'views/' => true,
+            APPPATH . 'views/'     => true
+        );
+
+        print '<pre>';
+        print_r($this->_ci_library_paths);
+        print_r($this->_ci_helper_paths);
+        print_r($this->_ci_model_paths);
+        print_r($this->_ci_view_paths);
+        print '</pre>';
     }
 }
