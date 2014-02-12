063c1170cfec4817c17f89b0dea3230f230f1177
Tim Whitacre <tim@argyllstudios.com>
2014-02-12 16:18:23 -0500

    Fixed issue in bookmarklet

--- application/views/marks/info.php
+++ application/views/marks/info.php
@@ -49,7 +49,9 @@
 <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
 <script src="/assets/js/nilai.js"></script>
 <script src="/assets/js/nilai.actions.js"></script>
+<script src="/assets/js/nilai.marks.js"></script>
 <script src="/assets/js/nilai.add.js"></script>
+<script src="/assets/js/nilai.init.js"></script>
 
 </body>
 </html>
