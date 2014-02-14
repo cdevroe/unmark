f55d6b73923e17079d83e96673c6b69d26690e22
Jeff Johns <phpfunk@gmail.com>
2014-02-14 14:48:21 -0500

    Fixing issue with custom routing

--- application/core/Plain_Router.php
+++ application/core/Plain_Router.php
@@ -102,4 +102,100 @@ class Plain_Router extends CI_Router
         // Re-index the segment array so that it starts with 1 rather than 0
         $this->uri->_reindex_segments();
     }
+
+    /**
+     * Validates the supplied segments.  Attempts to determine the path to
+     * the controller.
+     *
+     * @access  private
+     * @param   array
+     * @return  array
+     */
+    function _validate_request($segments)
+    {
+        if (count($segments) == 0)
+        {
+            return $segments;
+        }
+
+        // Does the requested controller exist in the root folder?
+        if (file_exists(APPPATH.'controllers/'.$segments[0].'.php') || file_exists(CUSTOMPATH.'controllers/'.$segments[0].'.php'))
+        {
+            return $segments;
+        }
+
+        // Is the controller in a sub-folder?
+        if (is_dir(APPPATH.'controllers/'.$segments[0]) || is_dir(CUSTOMPATH.'controllers/'.$segments[0]))
+        {
+            // Set the directory and remove it from the segment array
+            $this->set_directory($segments[0]);
+            $segments = array_slice($segments, 1);
+            $c_path   = (is_dir(APPPATH.'controllers/'.$segments[0])) ? APPPATH : CUSTOMPATH;
+
+            if (count($segments) > 0)
+            {
+                // Does the requested controller exist in the sub-folder?
+                if ( ! file_exists($c_path.'controllers/'.$this->fetch_directory().$segments[0].'.php'))
+                {
+                    if ( ! empty($this->routes['404_override']))
+                    {
+                        $x = explode('/', $this->routes['404_override']);
+
+                        $this->set_directory('');
+                        $this->set_class($x[0]);
+                        $this->set_method(isset($x[1]) ? $x[1] : 'index');
+
+                        return $x;
+                    }
+                    else
+                    {
+                        show_404($this->fetch_directory().$segments[0]);
+                    }
+                }
+            }
+            else
+            {
+                // Is the method being specified in the route?
+                if (strpos($this->default_controller, '/') !== FALSE)
+                {
+                    $x = explode('/', $this->default_controller);
+
+                    $this->set_class($x[0]);
+                    $this->set_method($x[1]);
+                }
+                else
+                {
+                    $this->set_class($this->default_controller);
+                    $this->set_method('index');
+                }
+
+                // Does the default controller exist in the sub-folder?
+                if ( ! file_exists($c_path.'controllers/'.$this->fetch_directory().$this->default_controller.'.php'))
+                {
+                    $this->directory = '';
+                    return array();
+                }
+
+            }
+
+            return $segments;
+        }
+
+
+        // If we've gotten this far it means that the URI does not correlate to a valid
+        // controller class.  We will now see if there is an override
+        if ( ! empty($this->routes['404_override']))
+        {
+            $x = explode('/', $this->routes['404_override']);
+
+            $this->set_class($x[0]);
+            $this->set_method(isset($x[1]) ? $x[1] : 'index');
+
+            return $x;
+        }
+
+
+        // Nothing else to do at this point but show a 404
+        show_404($segments[0]);
+    }
 }
