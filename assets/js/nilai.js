ed5922fb92e179d860be44b31f315bd01e61a773
Tim Whitacre <tim@argyllstudios.com>
2014-02-13 21:04:00 -0500

    Added cookie read/write function

--- assets/js/nilai.js
+++ assets/js/nilai.js
@@ -99,4 +99,27 @@ if (nilai === undefined) { var nilai = {}; }
         return (v === false || v === '' || v === null || v === 0 || v === undefined || l < 1);
     };
 
+    // Function to Create/Update Cookies
+    nilai.createCookie = function (name,value,days) {
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime()+(days*24*60*60*1000));
+            var expires = "; expires="+date.toGMTString();
+        }
+        else var expires = "";
+        document.cookie = name+"="+value+expires+"; path=/";
+    };
+
+    // Function to Read Cookie
+    nilai.readCookie = function (name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        }
+        return null;
+    };
+
 }(window.jQuery));
