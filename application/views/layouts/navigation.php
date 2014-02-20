1263da7089db6b8736358185fd5585f2529d1a8e
Tim Whitacre <tim@argyllstudios.com>
2014-02-20 14:17:49 -0500

    Updates to the account links and styles

--- application/views/layouts/navigation.php
+++ application/views/layouts/navigation.php
@@ -61,12 +61,7 @@
                 <li><a href="#" class="action" data-action="awesome">Turn On Awesome</a></li>
             </ul>
 
-            <h4 class="nav-heading">Account <span id="user-email">[ <?php print $user['email']; ?> ]</span></h4>
-            <ul class="nav-list">
-                <li><a href="#" class="action" data-action="change_password">Change Password</a></li>
-                <li><a href="#" class="action" data-action="change_email">Change Email Address</a></li>
-                <li><a href="mailto:?subject=Checkout Unmark&amp;body=You should really check out Unmark. http://unmark.it">Invite Others</a></li>
-            </ul>
+            <?php $this->load->view('layouts/accountlinks'); ?>
 
             <h4 class="nav-heading">Help</h4>
             <ul class="nav-list">
