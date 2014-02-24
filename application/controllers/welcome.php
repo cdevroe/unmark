f12fc5909978f79875d28db23c87130f279945d3
Jeff Johns <phpfunk@gmail.com>
2014-02-24 17:07:36 -0500

    Updated welcome index to show setup view when no DB is installed

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -11,20 +11,8 @@ class Welcome extends Plain_Controller
 	public function index()
 	{
 		$this->redirectIfLoggedIn('/marks');
-
-		// No user table
-		// Needs to be installed
-		// This should be updated to be cleaner
-		if (! $this->db->table_exists('users')) {
-			print_r('<p>Unmark has not been installed yet. Please <a href="/install/">install</a> before continuing.</p>');
-			exit;
-		}
-
-		$data['no_header'] = true;
-		$data['no_footer'] = true;
-
-		$this->view('welcome', $data);
-
+		$view = (! $this->db->table_exists('users')) ? 'setup' : 'welcome';
+		$this->view($view, array('no_header' => true, 'no_footer' => true));
 	}
 
 }
