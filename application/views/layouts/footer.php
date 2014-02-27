0304b6b656e7e97574f9b7325ce47e8e7f88a74b
Jakub Jakubiec <jjakubiec@gmail.com>
2014-02-27 13:54:41 +0100

    Redesigned footer to be re-usable between OS and Custom version

--- application/views/layouts/footer.php
+++ application/views/layouts/footer.php
@@ -4,21 +4,7 @@
     </div> <!-- end main-wrapper -->
 </div> <!-- end unmark-wrapper -->
 
-<?php $this->load->view('layouts/userforms'); ?>
-
-<?php $this->load->view('layouts/jsvars'); ?>
-
-<script src="/assets/libraries/jquery/jquery-2.1.0.min.js"></script>
-<script src="/assets/js/production/unmark.plugins.js"></script>
-<script src="/assets/js/production/unmark.loggedin.js"></script>
-
-
-<script type="text/javascript">
-$(document).ready(function() { 
-    unmark.createGraph(<?php print $stats['archived']['4 days ago'].", ".$stats['archived']['3 days ago'].", ".$stats['archived']['2 days ago'].", ".$stats['archived']['yesterday'].", ".$stats['archived']['today'].", ".$stats['saved']['4 days ago'].", ".$stats['saved']['3 days ago'].", ".$stats['saved']['2 days ago'].", ".$stats['saved']['yesterday'].", ".$stats['saved']['today']; ?>);
-});
-</script>
-
+<?php $this->load->view('layouts/footer_scripts')?>
 
 </body>
 </html>
