3abf1775a338e6e30a9b337d9a57b9466bcd4e03
Jakub Jakubiec <jjakubiec@gmail.com>
2014-03-03 13:47:43 +0100

    Refactored views - put common unlogged footer scripts to single partial.

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -43,15 +43,7 @@
 <div class="unmark-spinner"></div>
 <div class="unmark-success"><i class="icon-check"></i></div>
 
-
-<script type="text/javascript">
-var unmark  = unmark || {};
-unmark.vars = {};
-unmark.vars.csrf_token   = '<?php print $csrf_token; ?>';       
-</script>
-
-<script src="/assets/libraries/jquery/jquery-2.1.0.min.js"></script>
-<script src="/assets/js/production/unmark.loggedout.js"></script>
+<?php $this->load->view('layouts/footer_unlogged_scripts')?>
 
 </body>
 </html>
