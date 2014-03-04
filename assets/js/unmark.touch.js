2278723a07129b1dd6c8acaff7bb8d81d134ff24
Tim Whitacre <tim@argyllstudios.com>
2014-03-04 11:08:16 -0500

    Cleaned up some files and fixed some bugs, also started working on touch devices

---
+++ assets/js/unmark.touch.js
@@ -0,0 +1,16 @@
+/*!
+	Mobile & Touch Scripts for Unmark.it
+	Copyright 2014 - Plain - http://plainmade.com
+*/
+
+(function ($) {
+
+	$(document).ready(function () {
+
+		/*if (Modernizr.mq('only screen and (max-width: 320px)')) {
+			console.log('less than 480')
+		}*/
+
+	});
+
+}(window.jQuery));
