7eb7f12c4eb18ebcb46a5239a45a8bab9bfc8778
Colin Devroe <colin@cdevroe.com>
2014-03-05 17:55:25 -0500

    Fixed font SSL issue on welcome page.

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <title>Unmark</title>
-    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic|Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic|Montserrat:400,700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="/assets/css/unmark.css" />
     <link rel="stylesheet" href="/assets/css/unmark_welcome.css" />
     <link rel="icon" type="image/ico" href="/favicon.ico" />
