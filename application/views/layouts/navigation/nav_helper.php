900faa21d0459b40372cdf7121ec7878fb61ef80
Colin Devroe <colin@cdevroe.com>
2014-03-05 16:54:39 -0500

    Changed more instances of the word links to marks.

--- application/views/layouts/navigation/nav_helper.php
+++ application/views/layouts/navigation/nav_helper.php
@@ -1,4 +1,4 @@
-<li class="panel-marks"><a href="/marks">All Links</a></li>
+<li class="panel-marks"><a href="/marks">All Marks</a></li>
 <li class="panel-label"><a rel="200" href="#panel-label">Labels</a></li>
 <li class="panel-timeline"><a rel="250" href="#panel-timeline">Timeline</a></li>
 <li class="panel-search"><a rel="350" href="#panel-search">Search</a></li>
--- application/views/layouts/sidebar.php
+++ application/views/layouts/sidebar.php
@@ -4,7 +4,7 @@
             <?php if (isset($stats)) : ?>
             <p>
                 You saved <span class="ns-today"><?php print $stats['saved']['today']; ?></span>
-                links today and archived <span class="na-today"><?php print $stats['archived']['today']; ?></span>.
+                marks today and archived <span class="na-today"><?php print $stats['archived']['today']; ?></span>.
             </p>
             <?php endif; ?>
         </div>
@@ -12,8 +12,8 @@
     <?php if (isset($stats) && $stats['marks']['ages ago'] > 0) : ?>
     <div class="sidebar-block">
         <div class="sidebar-inner">
-            <p>You have <span class="ns-year"><?php print $stats['marks']['ages ago']; ?></span> links that are over 1 year old. Want to tidy up a bit?</p>
-            <a href="/marks/ages-ago" class="btn">View Links</a>
+            <p>You have <span class="ns-year"><?php print $stats['marks']['ages ago']; ?></span> marks that are over 1 year old. Want to tidy up a bit?</p>
+            <a href="/marks/ages-ago" class="btn">View Marks</a>
             <button data-action="dismiss_this">Do Nothing</button>
             <button data-action="archive_all">Archive All</button>
         </div>
@@ -23,7 +23,7 @@
     <div class="sidebar-block">
         <div class="sidebar-inner">
             <a href="javascript:(function()%7Bf%3D%27<?php print getFullUrl(); ?>/mark/add%3Furl%3D%27%2BencodeURIComponent(window.location.href)%2B%27%26title%3D%27%2BencodeURIComponent(document.title)%2B%27%26v%3D6%26%27%3Ba%3Dfunction()%7Bif(!window.open(f%2B%27noui%3D1%26jump%3Ddoclose%27,%27nilaiv2%27,%27location%3D1,links%3D0,scrollbars%3D0,toolbar%3D0,width%3D594,height%3D485%27))location.href%3Df%2B%27jump%3Dyes%27%7D%3Bif(/Firefox/.test(navigator.userAgent))%7BsetTimeout(a,0)%7Delse%7Ba()%7D%7D)()" class="btn">Unmark+</a>
-            <p class="clear sidenote">Drag this button into your bookmark toolbar.  <a href="http://help.unmark.it/bookmarklet" target="_blank">Learn how</a>.</p>
+            <p class="clear sidenote">To add Marks, drag this button into your bookmark toolbar.  <a href="http://help.unmark.it/bookmarklet" target="_blank">Learn how</a>.</p>
         </div>
     </div>
     <?php endif; ?>
