c338349a428b72d4a93cdc579f4ff024f5c01391
Colin Devroe <colin@cdevroe.com>
2014-03-06 09:50:42 -0500

    More font SSL issues.

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="apple-mobile-web-app-capable" content="yes" />
-    <title>Unmark</title>
+    <title>Unmark - The to do app for bookmarks.</title>
     <link href='//fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic|Montserrat:400,700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="/assets/css/unmark.css" />
     <link rel="stylesheet" href="/assets/css/unmark_welcome.css" />
