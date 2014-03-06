760d8ca13992e98efac99712fa3bf05a746b2d7e
Tim Whitacre <tim@argyllstudios.com>
2014-03-06 14:41:00 -0500

    Updates to login form... allow for password autofill and remembering

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -34,8 +34,8 @@
             <h1>Sign In To</h1>
             <div class="login-text"><img src="/assets/images/icons/logo_text_light.png" /></div>
             <form id="unmarkLogin" method="post" action="/login">
-                <input type="email" class="field-input" name="email" id="email" placeholder="Email Address" autocomplete="off" autocapitalize="off" autocorrect="off" />
-                <input type="password" class="field-input" name="password" id="password" placeholder="Password" autocomplete="off" />
+                <input type="email" class="field-input" name="email" id="email" placeholder="Email Address" />
+                <input type="password" class="field-input" name="password" id="password" placeholder="Password" />
                 <button class="login-submit" type="submit"><i class="icon-go"></i></button>
             </form>
             <div class="response-message"></div>
