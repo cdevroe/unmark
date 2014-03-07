c28134a0f0b35bf17b888727209c0d5acb389aa4
Tim Whitacre <tim@argyllstudios.com>
2014-03-07 13:02:01 -0500

    Updated sidebar and sidebar styles

--- application/views/layouts/sidebar/sidebar_notices.php
+++ application/views/layouts/sidebar/sidebar_notices.php
@@ -1,3 +1,21 @@
 <?php
 
 // Add any notification you want to appear in the sidebar here...
+
+
+?>
+
+<div class="sidebar-block">
+    <div class="sidebar-inner">
+        <p>Thanks for checking out Unmark. We hope you enjoy using it.</p>
+		<p>For usage tips follow us on Twitter! Please tell your friends about Unmark.</p>
+		<p>
+			<a href="https://twitter.com/unmarkit" class="twitter-follow-button" data-show-count="false" data-size="large" data-dnt="true">Follow @unmarkit</a>
+			<span class="sidebar-spacer"></span>
+			<a href="https://twitter.com/share" class="twitter-share-button" data-url="https://unmark.it" data-text="Checkout Unmark, the to do app for bookmarks." data-size="large" data-related="unmarkit" data-dnt="true">Tweet</a>
+			<script>
+			!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+			</script>
+		</p>
+    </div>
+</div>
