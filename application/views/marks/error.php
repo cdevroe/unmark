c1eb81eccf38fd6292dfc1711191512da3dce4d3
Tim Whitacre <tim@argyllstudios.com>
2014-03-10 13:20:59 -0400

    Update the marks/error page and changed some error copy

--- application/views/marks/error.php
+++ application/views/marks/error.php
@@ -1,3 +1,17 @@
-<pre>
-<?php print_r($errors); ?>
-</pre>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sorry, Could Not Add this Mark</title>
+	<link href='http://fonts.googleapis.com/css?family=Lato:300,400|Merriweather' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="/assets/css/unmark.css">
+	<link rel="icon" type="image/ico" href="/favicon.ico">
+</head>
+<body class="unmark-solo" id="unmark-login">
+	<div id="error-wrapper">
+		<div id="error-icon"><img src="/assets/images/icons/large_x.png" /></div>
+		<h1>Yikes!</h1>
+		<p><?php print current($errors); ?></p>
+		<p>This mark could not be added to your stream.</p>
+	</div>
+</body>
+</html>
