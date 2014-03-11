f999b4ea5fbbd6bf754ba65497be66cff8f8d699
Tim Whitacre <tim@argyllstudios.com>
2014-03-11 12:01:46 -0400

    Hid action buttons on nav or sidebar interaction

--- assets/js/unmark.touch.js
+++ assets/js/unmark.touch.js
@@ -15,6 +15,7 @@
 				$('.navigation-content .menu-activator').animate({left: 62}, 400);
 				$('.menu-item').removeClass('active-menu');
 			} else {
+				$('.mark-actions').hide();
 				$('.main-wrapper').animate({left: 65}, 400);
 				$('.navigation-content').animate({left: 0}, 400);
 				$('.navigation-content .menu-activator').animate({left: 0}, 400);
