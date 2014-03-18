19fd5071061e925cb02de3f4325dcda52aa13bf6
Tim Whitacre <tim@argyllstudios.com>
2014-03-18 11:53:40 -0400

    Removed 'apple-mobile-web-app-capable' tag from header

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <meta name="apple-mobile-web-app-capable" content="yes" />
     <title>Unmark</title>
     <link href='//fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic|Merriweather:400,300,300italic,400italic,700,700italic|Montserrat:400,700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="/assets/css/unmark.css" />
