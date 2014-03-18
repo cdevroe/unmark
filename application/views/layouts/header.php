87eac948311778432f8e956368a5f2731f2619a8
Tim Whitacre <tim@argyllstudios.com>
2014-03-18 12:15:53 -0400

    Fixed some mobile styles

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black"/>
     <meta name="apple-mobile-web-app-capable" content="no">
     <title>Unmark</title>
     <link href='//fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic|Merriweather:400,300,300italic,400italic,700,700italic|Montserrat:400,700' rel='stylesheet' type='text/css'>
