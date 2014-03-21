9b510461c95dc9d1c5d84d2dec2fce799edfcb2f
Jeff Johns <phpfunk@gmail.com>
2014-03-21 10:17:00 -0400

    Fixed issue with embedly script

--- application/controllers/cron.php
+++ application/controllers/cron.php
@@ -40,38 +40,11 @@ class Cron extends Plain_Controller
 
         if (isset($marks[0]->mark_id)) {
             $this->load->helper('oembed');
-            //$this->load->helper('hrecipe');
-
-            // Get all system smart label domains for food & drink
-            $this->load->model('labels_model', 'label');
-            $label = $this->label->read("labels.slug = 'eat-drink'", 1, 1, 'label_id');
-
-            // Set all eat/drink domains to array
-            /*$recipe_domains = array();
-            if (isset($label->label_id) && is_numeric($label->label_id)) {
-                $smart_labels = $this->label->read("labels.smart_label_id = '" . $label->label_id . "' AND labels.user_id IS NULL", 'all', 1, 'domain');
-                if (isset($smart_labels[0]->domain)) {
-                    foreach ($smart_labels as $k => $obj) {
-                        array_push($recipe_domains, str_replace('www.', '', strtolower($obj->domain)));
-                    }
-                }
-            }*/
-
             foreach($marks as $k => $mark) {
 
                 // OEmbed check
                 // If no embed, check recipes
                 $embed = oembed($mark->url, $embedly_key);
-
-                // parse_url for host
-                // Check if in recipe domain list
-                // If so, try to find a recipe
-                /*if (empty($embed)) {
-                    $domain = str_replace('www.', '', strtolower(parse_url($mark->url,  PHP_URL_HOST)));
-                    if (! empty($domain) && in_array($domain, $recipe_domains)) {
-                        $embed = parse_hrecipe($mark->url);
-                    }
-                }*/
 
                 // Set options
                 // Set embed processed = 1
