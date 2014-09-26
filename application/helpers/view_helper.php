81ceccea3189432a7c5ec01eaaab49417bd7de2b
Kyle Ruane <kyle.j.ruane@gmail.com>
2014-09-26 16:29:06 -0400

    Updated spacing on search archive link

--- application/helpers/view_helper.php
+++ application/helpers/view_helper.php
@@ -1,7 +1,7 @@
 <?php defined("BASEPATH") or exit("No direct script access allowed");
 
 // Asset File Version Number
-define("ASSET_VERSION", "1411762154170");
+define("ASSET_VERSION", "1411763312977");
 
 // Prints marks count in nice format
 function printMarksCount($num)
