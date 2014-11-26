7e842beb6dfbcd02d9d71f429ebc1ea31ece326b
Colin Devroe <colin@cdevroe.com>
2014-11-26 12:57:59 -0500

    Fixed a bug with tag search extending into the archive. Addresses #65

--- application/views/marks/index.php
+++ application/views/marks/index.php
@@ -2,6 +2,8 @@
 <h2 class="marks-heading"><?php echo _('Sorry, No marks found')?></h2>
 <?php if (isset($_GET['q']) & $lookup_type == "search") : // Only if this is for a search ?>
 <div class="marks continue-search no-results"><?php echo sprintf( _('Would you like to <a href="/marks/archive/search?q=%s">try searching your archive</a>?'), (isset($_GET['q'])) ? $_GET['q'] : ''); ?></div>
+<?php elseif ($lookup_type == 'tag') : ?>
+<div class="marks continue-search no-results"><?php echo sprintf( _('Would you like to <a href="/marks/archive/search?q=%s">try searching your archive</a>?'), $active_tag['tag_name']); ?></div>
 <?php endif; ?>
 <?php else : ?>
 <?php if (isset($lookup_type) && $lookup_type != "all") :
