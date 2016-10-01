cb0200b814d6593bb66d0679b36de40c5faaf49e
Colin Devroe <colin@cdevroe.com>
2016-10-01 11:26:44 -0400

    Cleaned up phrase array. Formatting, capitalization, etc. Added new phrases

--- application/views/layouts/navigation/nav_panels.php
+++ application/views/layouts/navigation/nav_panels.php
@@ -41,5 +41,5 @@
 		<li><a href="javascript:(function()%7Bf%3D%27<?php print rtrim(base_url(),'/'); ?>%2Fmark/add%3Furl%3D%27%2BencodeURIComponent(window.location.href)%2B%27%26title%3D%27%2BencodeURIComponent(document.title)%2B%27%26v%3D6%26%27%3Ba%3Dfunction()%7Bif(!window.open(f%2B%27noui%3D1%26jump%3Ddoclose%27,%27nilaiv2%27,%27location%3D1,links%3D0,scrollbars%3D0,toolbar%3D0,width%3D594,height%3D485%27))location.href%3Df%2B%27jump%3Dyes%27%7D%3Bif(/Firefox/.test(navigator.userAgent))%7BsetTimeout(a,0)%7Delse%7Ba()%7D%7D)();">Save to Unmark</a></li>
 		<li><a target="_blank" href="https://chrome.google.com/webstore/detail/unmark/cdhnljlbeehjgddokagghpfgahhlifch"><?php echo _('Get the Chrome Extension') ?></a></li>
 	</ul>
-	<p style="text-align: center;">Version <?php echo $this->config->item('unmark_version'); ?></p>
+	<p style="text-align: center;"><?php echo _('Version'); ?> <?php echo $this->config->item('unmark_version'); ?></p>
 </div>
