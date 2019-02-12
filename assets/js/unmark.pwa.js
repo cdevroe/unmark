216962143aa058f6472ef752155b5edd3ceed4c9
Colin Devroe <colin@cdevroe.com>
2019-02-12 17:53:21 -0500

    Added basic PWAs support. Service worker. Manifest file. Icons. And some code for adding marks from apps on mobile.

---
+++ assets/js/unmark.pwa.js
@@ -0,0 +1,11 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
