d2552bf848afba4a856b8be7678e82e8f8dc742f
Colin Devroe <colin@cdevroe.com>
2019-02-14 10:05:11 -0500

    Updated URLs for static files for import

--- application/views/import/index.php
+++ application/views/import/index.php
@@ -1,9 +1,9 @@
 <html>
 <head>
     <title><?php echo unmark_phrase('Unmark : Importer'); ?></title>
-    <link href='http://fonts.googleapis.com/css?family=Lato:300,400|Merriweather' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="assets/css/unmark.css" />
-    <link rel="icon" type="image/ico" href="favicon.ico" />
+    <link href='//fonts.googleapis.com/css?family=Lato:300,400|Merriweather' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="/assets/css/unmark.css" />
+    <link rel="icon" type="image/ico" href="/favicon.ico" />
 </head>
 <body class="unmark-solo" id="unmark-login">
 <div id="unmark_login_page">
@@ -34,8 +34,8 @@
     </div>
 </div>
 
-<script src="assets/libraries/jquery/jquery-2.1.0.min.js"></script>
-<script src="assets/js/production/unmark.loggedout.js"></script>
+<script src="/assets/libraries/jquery/jquery-2.1.0.min.js"></script>
+<script src="/assets/js/production/unmark.loggedout.js"></script>
 
 </body>
 </html>
