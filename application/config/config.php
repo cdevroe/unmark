c52e5778d1c516a0a9a434e3907e043e2c156d7b
Colin Devroe <colin@cdevroe.com>
2019-02-22 14:07:58 -0500

    Bumped version number

--- application/config/config.php
+++ application/config/config.php
@@ -11,7 +11,7 @@
 | version.point.release
 |
 */
-$config['unmark_version'] = '1.8.05';
+$config['unmark_version'] = '1.8.06';
 
 /*
 |--------------------------------------------------------------------------
