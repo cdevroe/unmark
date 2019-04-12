7928ed829e2d41b75d0aa4b9c693d30f00748202
Kyle Ruane <kyle.j.ruane@gmail.com>
2019-04-12 10:18:25 -0400

    Add remove button to tag pills

--- application/views/layouts/footer.php
+++ application/views/layouts/footer.php
@@ -12,6 +12,7 @@
 <script>
   $(document).ready(function() {
     $('#input-tags').selectize({
+        plugins: ['remove_button'],
         delimiter: ',',
         persist: false,
         create: function(input) {
