8bba0dd3947f2265cae8e9892a783cc9b9f539c6
Colin Devroe <colin@cdevroe.com>
2025-08-22 08:42:33 -0400

    Updated Docker setup to run PHP 7.4. Updated license. Versioned.

--- application/config/config.php
+++ application/config/config.php
@@ -11,7 +11,7 @@
 | version.point.release
 |
 */
-$config['unmark_version'] = '2020.4';
+$config['unmark_version'] = '2025.1';
 
 /*
 |--------------------------------------------------------------------------
